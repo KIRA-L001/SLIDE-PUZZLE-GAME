ff40416b4c4d73f5e3b37e2e1832e0f696be4bc8
Dibakar <137947115+KIRA-L001@users.noreply.github.com>
2024-01-28 20:59:26 +0530

    Delete SLIDE PUZZlE GAME directory

--- SLIDE PUZZlE GAME/leaderboard_script.js
+++
@@ -1,9 +0,0 @@
-function nextPage1(){
-    window.location.href = "index.html";
-}
-function nextPage2(){
-    window.location.href = "level_index.html";
-}
-function nextPage3(){
-    window.location.href = "contacts_index.html";
-}
--- SLIDE PUZZlE GAME/win_script.js
+++
@@ -1,6 +0,0 @@
-function nextPage1(){
-    window.location.href = "index.html";
-}
-function nextPage2(){
-    window.location.href = "level_index.html";
-}
