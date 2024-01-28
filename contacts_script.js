c8d27174e41c354d7ad81aa902e6c0dd84c346a7
Dibakar <137947115+KIRA-L001@users.noreply.github.com>
2024-01-28 21:02:36 +0530

    adding initial files

---
+++ contacts_script.js
@@ -0,0 +1,12 @@
+function nextPage1(){
+    window.location.href = "index.html";
+}
+function nextPage2(){
+    window.location.href = "level_index.html";
+}
+function nextPage3(){
+    window.location.href = "leaderboard_index.html";
+}
+function next(){
+    window.location.href = "level_index.html";
+}
---
+++ leaderboard_script.js
@@ -0,0 +1,9 @@
+function nextPage1(){
+    window.location.href = "index.html";
+}
+function nextPage2(){
+    window.location.href = "level_index.html";
+}
+function nextPage3(){
+    window.location.href = "contacts_index.html";
+}
